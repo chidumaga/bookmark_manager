607c1677e441d337382c4f4b0d35615cd35ce20f
Makers Laptop 66 <chidumaga@gmail.com>
2015-05-19 15:41:47 +0100

    Password confirmation

--- server.rb
+++ server.rb
@@ -7,12 +7,16 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
 require './lib/tag'
+require './lib/user'
 
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     @links = Link.all
     erb :index
@@ -33,8 +37,24 @@ class BookmarkManager < Sinatra::Base
     erb :index
   end
 
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+
+  def current_user
+    @current_user ||= User.get(session[:user_id]) if session[:user_id]
+  end
+
+end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
-
-
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,15 +1,22 @@
 feature 'User signs in' do
   scenario 'when entering the site' do
     expect { sign_up }.to change(User, :count).by(1)
+
+
     expect(page).to have_content('Welcome, chidu@chidu.com')
     expect(User.first.email).to eq('chidu@chidu.com')
   end
 
-  def sign_up (email = 'chidu@chidu.com', password = 'hey123')
+  scenario 'user fills in wrong password' do
+    expect { sign_up 'j@j.com', 'hey123', 'hey124' }.to change(User, :count).by(0)
+  end
+
+  def sign_up (email = 'chidu@chidu.com', password = 'hey123', password_confirmation = 'hey123')
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email, with: email
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
   end
 end
